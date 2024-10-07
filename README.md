eeee9c7910a5840580ad3e040bc6fad3b24686fe
Joshua Burt <Joshua.Burt@MyGeorgian.ca>
2024-10-07 15:11:50 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Kotlin-MongoDB_LibrarySystem
 Kotlin with a local MongoDB connection
+
+//Local MongoDB Connection - only you have access to data, it is saved after each session
+Steps: 
+1. New Project -> Kotlin; Build System -> Gradle
+2. Download MongoDB Compass
+3. Next to 'Connections'; click + (Add new connection)
+4. Enter the connection string i.e. mongodb://localhost:27017
--- src/main/kotlin/MongoDBManager.kt
+++ src/main/kotlin/MongoDBManager.kt
@@ -5,13 +5,6 @@ import com.mongodb.client.MongoCollection
 import com.mongodb.client.MongoDatabase
 import org.bson.Document
 
-//Local MongoDB Connection - only you have access to data, it is saved after each session
-//New Project -> Kotlin; Build System -> Gradle
-
-//Download MongoDB Compass
-//Next to 'Connections'; click + (Add new connection)
-//Enter the connection string i.e. mongodb://localhost:27017
-
 class MongoDBManager {
     private val client = MongoClients.create("mongodb://localhost:27017") // Adjust connection string if necessary
     private val database: MongoDatabase = client.getDatabase("librarySystem")
